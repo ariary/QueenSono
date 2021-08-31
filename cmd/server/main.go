fa0609b1d31475a106e70a09d1ed43d202bedfad
ariary <ariary9.2@hotmail.fr>
2021-08-31 17:38:19 +0200

    Update main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -20,6 +20,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 
 	"github.com/ariary/QueenSono/pkg/icmp"
 	"github.com/spf13/cobra"
@@ -63,23 +64,43 @@ it uses the icmp protocol.`,
 		},
 	}
 
-	// var cmdTruncated = &cobra.Command{
-	// 	Use:   "truncated [delay]",
-	// 	Short: "receive data from icmp packet and do not wait indefinitively for all the packet.",
-	// 	Long:  `rreceive data from icmp packet and do not wait indefinitively for all the packet. Hence you could receive truncated data`,
-	// 	Args:  cobra.MinimumNArgs(1),
-	// 	Run: func(cmd *cobra.Command, args []string) {
+	var cmdTruncated = &cobra.Command{
+		Use:   "truncated [delay]",
+		Short: "receive data from icmp packet and do not wait indefinitively for all the packet.",
+		Long:  `rreceive data from icmp packet and do not wait indefinitively for all the packet. Hence you could receive truncated data`,
+		Args:  cobra.MinimumNArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
 
-	// 		size, sender := icmp.GetMessageSizeAndSender(listenAddr)
-	// 		fmt.Println("Sender:", sender, ", Number of packet wanted:", size)
-	// 		message := make(chan string)
-	// 		//go countdown(strconv.Atoi(args[0]),size)
-	// 		go icmp.ServeTemporary(listenAddr, size, message)
-	// 		fmt.Println("Message received:", message)
-	// 		//icmp.SendHashedmessage(message, sender) //Integrity check
+			size, sender := icmp.GetMessageSizeAndSender(listenAddr)
+			fmt.Println("Sender:", sender, ", Number of packet wanted:", size)
+			data := make(chan string)
+			//go countdown(strconv.Atoi(args[0]),size)
+			delay, err := strconv.Atoi(args[0])
+			if err != nil {
+				panic(err)
+			}
+			icmp.ServeTemporary(listenAddr, size, delay, data)
+			message := <-data
+			fmt.Println("Message received:", message)
+			//icmp.SendHashedmessage(message, sender) //Integrity check
 
-	// 	},
-	// }
+			if filename != "" {
+				f, err := os.Create(filename)
+				if err != nil {
+					log.Fatal(err)
+				}
+				defer f.Close()
+
+				_, err2 := f.WriteString(message)
+				if err2 != nil {
+					log.Fatal(err2)
+				}
+				fmt.Println("data saved in", filename)
+			} else {
+				fmt.Println("qsreceiver received:", message)
+			}
+		},
+	}
 
 	//cmdReceive flag handling
 	cmdReceive.PersistentFlags().StringVarP(&listenAddr, "listen", "l", "0.0.0.0", "address used for listening icmp packet")
@@ -88,6 +109,6 @@ it uses the icmp protocol.`,
 
 	var rootCmd = &cobra.Command{Use: "qsreceiver"}
 	rootCmd.AddCommand(cmdReceive)
-	// cmdReceive.AddCommand(cmdTruncated)
+	cmdReceive.AddCommand(cmdTruncated)
 	rootCmd.Execute()
 }
