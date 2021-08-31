1e10b7ea65c288eb2eaff49dc00230c5cee59ced
ariary <ariary9.2@hotmail.fr>
2021-08-31 15:54:08 +0200

    Update main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,155 +1,93 @@
-package icmp
+// package main
+
+// import (
+// 	"fmt"
+
+// 	"github.com/ariary/QueenSono/pkg/icmp"
+// )
+
+// func main() {
+// 	listenAddr := "10.0.2.15"
+// 	size, sender := icmp.GetMessageSizeAndSender(listenAddr)
+// 	fmt.Println("Sender:", sender, ", Number of packet wanted:", size)
+// 	message := icmp.Serve(listenAddr, size)
+// 	fmt.Println("Message received:", message)
+// 	//icmp.SendHashedmessage(message, sender) //Integrity check
+// }
+package main
 
 import (
 	"fmt"
+	"log"
 	"os"
-	"strconv"
 
-	"github.com/schollz/progressbar/v3"
-	"golang.org/x/net/icmp"
-	"golang.org/x/net/ipv4"
+	"github.com/ariary/QueenSono/pkg/icmp"
+	"github.com/spf13/cobra"
 )
 
-//Wait for the fistr ICMP packet setting sized of data
-func GetMessageSizeAndSender(listenAddr string) (size int, sender string) {
-	c, err := icmp.ListenPacket("ip4:icmp", listenAddr)
-	if err != nil {
-		fmt.Println("Error:", err)
-	}
-	defer c.Close()
+func main() {
+	//TODO truncated,integrity,crossbar,
+	//CMD RECEIVE
+	//receive var
+	var listenAddr string
+	var filename string
+	var progressBar bool
 
-	packet := make([]byte, 65507)
-	n, peer, err := c.ReadFrom(packet)
-	if err != nil {
-		fmt.Println("Error while reading icmp packet:", err)
-	}
+	var cmdReceive = &cobra.Command{
+		Use:   "receive",
+		Short: "receive data from icmp packet",
+		Long: `receive is for receiving  data from a remote queensono sender.
+it uses the icmp protocol.`,
+		Args: cobra.MinimumNArgs(0),
+		Run: func(cmd *cobra.Command, args []string) {
 
-	message, err := icmp.ParseMessage(ProtocolICMP, packet[:n])
-	if err != nil {
-		fmt.Println("Error while parsing icmp message:", err)
-	}
+			size, sender := icmp.GetMessageSizeAndSender(listenAddr)
+			fmt.Println("Sender:", sender, ", Number of packet wanted:", size)
+			message := icmp.Serve(listenAddr, size, progressBar)
+			//icmp.SendHashedmessage(message, sender) //Integrity check
+			if filename != "" {
+				f, err := os.Create(filename)
+				if err != nil {
+					log.Fatal(err)
+				}
+				defer f.Close()
 
-	switch message.Type {
-	case ipv4.ICMPTypeEcho:
-		echo, _ := message.Body.Marshal(1)
-		m := string(echo[4:]) //clean
-		size, err = strconv.Atoi(m)
-		if err != nil {
-			fmt.Println(err)
-		}
-	default:
-		fmt.Errorf("got %+v from %v; want echo request", message, peer)
-	}
-	sender = peer.String()
-	return size, sender
-}
-
-//ICMP server waiting for packet (waiting n packet)
-func Serve(listenAddr string, n int, progressBar bool) (data string) {
-	var bar *progressbar.ProgressBar
-	if progressBar {
-		bar = progressbar.Default(int64(n))
-	}
-	c, err := icmp.ListenPacket("ip4:icmp", listenAddr)
-	if err != nil {
-		fmt.Println("Error:", err)
-	}
-	defer c.Close()
-	for i := 0; i < n; i++ {
-		packet := make([]byte, 65507)
-		n, peer, err := c.ReadFrom(packet)
-		if err != nil {
-			fmt.Println("Error while reading icmp packet:", err)
-		}
-
-		message, err := icmp.ParseMessage(ProtocolICMP, packet[:n])
-		if err != nil {
-			fmt.Println("Error while parsing icmp message:", err)
-		}
-
-		switch message.Type {
-		case ipv4.ICMPTypeEcho:
-			echo, _ := message.Body.Marshal(1)
-			m := string(echo[2:]) //clean
-			if !progressBar {
-				fmt.Println(m)
+				_, err2 := f.WriteString(message)
+				if err2 != nil {
+					log.Fatal(err2)
+				}
+				fmt.Println("data saved in", filename)
+			} else {
+				fmt.Println("qsreceiver received:", message)
 			}
-		default:
-			fmt.Errorf("got %+v from %v; want echo request", message, peer)
-		}
-		if progressBar {
-			bar.Add(1)
-		}
-
+		},
 	}
-	return data
-}
 
-//ICMP server waiting for specific number of packet (waiting n packet)
-// func ServeTemporary(listenAddr string, n int, data chan string) {
-// 	c, err := icmp.ListenPacket("ip4:icmp", listenAddr)
-// 	if err != nil {
-// 		fmt.Println("Error:", err)
-// 	}
-// 	defer c.Close()
-// 	for i := 0; i < n; i++ {
-// 		packet := make([]byte, 65507)
-// 		n, peer, err := c.ReadFrom(packet)
-// 		if err != nil {
-// 			fmt.Println("Error while reading icmp packet:", err)
-// 		}
+	// var cmdTruncated = &cobra.Command{
+	// 	Use:   "truncated [delay]",
+	// 	Short: "receive data from icmp packet and do not wait indefinitively for all the packet.",
+	// 	Long:  `rreceive data from icmp packet and do not wait indefinitively for all the packet. Hence you could receive truncated data`,
+	// 	Args:  cobra.MinimumNArgs(1),
+	// 	Run: func(cmd *cobra.Command, args []string) {
 
-// 		message, err := icmp.ParseMessage(ProtocolICMP, packet[:n])
-// 		if err != nil {
-// 			fmt.Println("Error while parsing icmp message:", err)
-// 		}
+	// 		size, sender := icmp.GetMessageSizeAndSender(listenAddr)
+	// 		fmt.Println("Sender:", sender, ", Number of packet wanted:", size)
+	// 		message := make(chan string)
+	// 		//go countdown(strconv.Atoi(args[0]),size)
+	// 		go icmp.ServeTemporary(listenAddr, size, message)
+	// 		fmt.Println("Message received:", message)
+	// 		//icmp.SendHashedmessage(message, sender) //Integrity check
 
-// 		switch message.Type {
-// 		case ipv4.ICMPTypeEcho:
-// 			echo, _ := message.Body.Marshal(1)
-// 			m := string(echo[2:]) //clean
-// 			fmt.Println(m)
-// 			data <- m
-// 		default:
-// 			fmt.Errorf("got %+v from %v; want echo request", message, peer)
-// 		}
-// 	}
-// 	return data
-// }
+	// 	},
+	// }
 
-//Wait ICMP message from remote to assert if the message is well received
-func IntegrityCheck(hash string) {
-	fmt.Println("launch integrity server")
-	c, err := icmp.ListenPacket("ip4:icmp", "localhost")
-	if err != nil {
-		fmt.Println("Error:", err)
-	}
-	defer c.Close()
+	//cmdReceive flag handling
+	cmdReceive.PersistentFlags().StringVarP(&listenAddr, "listen", "l", "0.0.0.0", "address used for listening icmp packet")
+	cmdReceive.PersistentFlags().StringVarP(&filename, "filename", "f", "", "filename where stored the data received")
+	cmdReceive.PersistentFlags().BoolVarP(&progressBar, "progress-bar", "p", false, "print progression of the data reception")
 
-	for {
-		packet := make([]byte, 65507)
-		n, peer, err := c.ReadFrom(packet)
-		if err != nil {
-			fmt.Println("Error while reading icmp packet:", err)
-		}
-
-		message, err := icmp.ParseMessage(ProtocolICMP, packet[:n])
-		if err != nil {
-			fmt.Println("Error while parsing icmp message:", err)
-		}
-
-		switch message.Type {
-		case ipv4.ICMPTypeEcho:
-			fmt.Println("Get integrity")
-			echo, _ := message.Body.Marshal(1)
-			fmt.Println("hash received:", string(echo[4:])) //clean
-			if string(echo[4:]) == hash {
-				fmt.Println("Communication end")
-				os.Exit(0)
-			}
-		default:
-			fmt.Println("DEFAULT!!!!!!!!!!")
-			fmt.Errorf("got %+v from %v; want echo request", message, peer)
-		}
-	}
+	var rootCmd = &cobra.Command{Use: "qsreceiver"}
+	rootCmd.AddCommand(cmdReceive)
+	// cmdReceive.AddCommand(cmdTruncated)
+	rootCmd.Execute()
 }
