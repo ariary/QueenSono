4e5f7c81e938aa595f331759a9b988ae90846a71
ariary <ariary9.2@hotmail.fr>
2021-08-31 15:23:34 +0200

    Update receive.go

--- pkg/icmp/receive.go
+++ pkg/icmp/receive.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"strconv"
 
+	"github.com/schollz/progressbar/v3"
 	"golang.org/x/net/icmp"
 	"golang.org/x/net/ipv4"
 )
@@ -44,7 +45,11 @@ func GetMessageSizeAndSender(listenAddr string) (size int, sender string) {
 }
 
 //ICMP server waiting for packet (waiting n packet)
-func Serve(listenAddr string, n int) (data string) {
+func Serve(listenAddr string, n int, progressBar bool) (data string) {
+	var bar *progressbar.ProgressBar
+	if progressBar {
+		bar = progressbar.Default(int64(n))
+	}
 	c, err := icmp.ListenPacket("ip4:icmp", listenAddr)
 	if err != nil {
 		fmt.Println("Error:", err)
@@ -66,14 +71,51 @@ func Serve(listenAddr string, n int) (data string) {
 		case ipv4.ICMPTypeEcho:
 			echo, _ := message.Body.Marshal(1)
 			m := string(echo[2:]) //clean
-			fmt.Println(m)
-			data += m
+			if !progressBar {
+				fmt.Println(m)
+			}
 		default:
 			fmt.Errorf("got %+v from %v; want echo request", message, peer)
 		}
+		if progressBar {
+			bar.Add(1)
+		}
+
 	}
 	return data
 }
+
+//ICMP server waiting for specific number of packet (waiting n packet)
+// func ServeTemporary(listenAddr string, n int, data chan string) {
+// 	c, err := icmp.ListenPacket("ip4:icmp", listenAddr)
+// 	if err != nil {
+// 		fmt.Println("Error:", err)
+// 	}
+// 	defer c.Close()
+// 	for i := 0; i < n; i++ {
+// 		packet := make([]byte, 65507)
+// 		n, peer, err := c.ReadFrom(packet)
+// 		if err != nil {
+// 			fmt.Println("Error while reading icmp packet:", err)
+// 		}
+
+// 		message, err := icmp.ParseMessage(ProtocolICMP, packet[:n])
+// 		if err != nil {
+// 			fmt.Println("Error while parsing icmp message:", err)
+// 		}
+
+// 		switch message.Type {
+// 		case ipv4.ICMPTypeEcho:
+// 			echo, _ := message.Body.Marshal(1)
+// 			m := string(echo[2:]) //clean
+// 			fmt.Println(m)
+// 			data <- m
+// 		default:
+// 			fmt.Errorf("got %+v from %v; want echo request", message, peer)
+// 		}
+// 	}
+// 	return data
+// }
 
 //Wait ICMP message from remote to assert if the message is well received
 func IntegrityCheck(hash string) {
